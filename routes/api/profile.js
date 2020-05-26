00b646629435dfe1f8fe10367fc969b2f4c24279
lironezra <lironezra1@gmail.com>
2020-05-26 12:34:44 +0300

    added Delete Profile Experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -205,4 +205,23 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    // Get the profile we want delete experience from
+    const foundProfile = await Profile.findOne({ user: req.user.id });
+
+    foundProfile.experience = foundProfile.experience.filter(
+      (exp) => exp._id.toString() !== req.params.exp_id
+    );
+
+    await foundProfile.save();
+    return res.status(200).json(foundProfile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
