83dfd6c3607d416b9eb0dba28a4a086038d1253e
lironezra <lironezra1@gmail.com>
2020-06-24 17:43:28 +0300

    added cors to server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const morgan = require("morgan");
-//const cors = require("cors");
-const bodyParser = require("body-parser");
-const connectDB = require("./config/db");
-const passport = require("passport");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const passport = require('passport');
 
-const { port } = require("./config");
+const { port } = require('./config');
 
 const app = express();
 
@@ -15,19 +15,19 @@ connectDB();
 // Init Middlewares
 app.use(express.json({ extended: false }));
 app.use(passport.initialize());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 // allow your application to be consumed
-// app.use(cors());
+app.use(cors());
 
 // Routes
-app.get("/", (req, res) => res.send("API Running"));
+app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
-app.use("/api/users", require("./routes/api/users"));
+app.use('/api/users', require('./routes/api/users'));
 // app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = port || 3000;
 
