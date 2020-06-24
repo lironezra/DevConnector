a14664c87a9a352e11a2157724d621db53cdb713
lironezra <lironezra1@gmail.com>
2020-06-24 19:21:51 +0300

    use the loadUser functionallity when the App mounts in first time for checking if the user still authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/navbar.component';
@@ -9,10 +9,20 @@ import Alert from './components/layout/alert/alert.component';
 //Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { loadUser } from './redux/auth/auth.actions';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
