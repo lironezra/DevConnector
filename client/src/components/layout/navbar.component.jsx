93f7fdcec78dde2ff3e251e424bdd2950217d0da
lironezra <lironezra1@gmail.com>
2020-06-25 16:52:37 +0300

    edit navbar links for authenticated/guest users

--- client/src/components/layout/navbar.component.jsx
+++ client/src/components/layout/navbar.component.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../../redux/auth/auth.actions';
 
 const Navbar = () => {
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={() => dispatch(logout())} to='#!'>
+          <i className='fas fa-sign-out-alt'> </i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to='#!'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,17 +39,7 @@ const Navbar = () => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to='profiles.html'>Developers</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      {!loading && <>{isAuthenticated ? authLinks : guestLinks}</>}
     </nav>
   );
 };
