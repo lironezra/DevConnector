bccab31ea6026de077041119b7442e8891801f64
lironezra <lironezra1@gmail.com>
2020-06-25 18:05:36 +0300

    created dashboard component, added private route component and use it for the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/routing/private-route.component';
 
 import Navbar from './components/layout/navbar.component';
 import LandingPage from './components/layout/landing-page.component';
 import Login from './components/auth/login.component';
 import Register from './components/auth/register.component';
 import Alert from './components/layout/alert/alert.component';
+import Dashboard from './components/dashboard/dashboard.component';
 //Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </>
