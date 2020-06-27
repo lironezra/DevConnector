d0082c8d41baaa9bc8d29dc6413ff519f8ef41b5
lironezra <lironezra1@gmail.com>
2020-06-27 16:28:37 +0300

    added link to navbar

--- client/src/components/layout/navbar.component.jsx
+++ client/src/components/layout/navbar.component.jsx
@@ -10,6 +10,9 @@ const Navbar = () => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
@@ -27,7 +30,7 @@ const Navbar = () => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='#!'>Developers</Link>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
