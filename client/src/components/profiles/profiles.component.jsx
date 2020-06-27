ef36f09f5554073dae1befcf71066a18ad1b8ba9
lironezra <lironezra1@gmail.com>
2020-06-27 16:29:02 +0300

    added Profiles anf ProfileItem components

---
+++ client/src/components/profiles/profiles.component.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProfiles } from '../../redux/profile/profile.actions';
+
+import Spinner from '../layout/spinner/spinner.component';
+import ProfileItem from './profile-item.component';
+
+const Profiles = () => {
+  const dispatch = useDispatch();
+  const { profiles, loading } = useSelector((state) => state.profile);
+
+  useEffect(() => {
+    dispatch(getAllProfiles());
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop'></i> Browse and connect with
+            developers
+          </p>
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map((profile) => {
+                return <ProfileItem key={profile._id} profile={profile} />;
+              })
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Profiles;
