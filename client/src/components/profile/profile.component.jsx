244fbc517c5893750f363e04423b1ad27c63e11e
lironezra <lironezra1@gmail.com>
2020-06-27 19:11:25 +0300

    added profile-top and profile-about sections to profile component

--- client/src/components/profile/profile.component.jsx
+++ client/src/components/profile/profile.component.jsx
@@ -5,6 +5,8 @@ import { Link, withRouter } from 'react-router-dom';
 import { getProfileById } from '../../redux/profile/profile.actions';
 
 import Spinner from '../layout/spinner/spinner.component';
+import ProfileTop from './profile-top.component';
+import ProfileAbout from './profile-about.component';
 
 const Profile = ({ match }) => {
   const dispatch = useDispatch();
@@ -17,7 +19,7 @@ const Profile = ({ match }) => {
 
   useEffect(() => {
     dispatch(getProfileById(match.params.id));
-  }, [dispatch]);
+  }, [dispatch, match.params.id]);
 
   return (
     <>
@@ -37,34 +39,8 @@ const Profile = ({ match }) => {
             )}
 
           <div className='profile-grid my-1'>
-            {/* <div className='profile-top bg-primary p-2'>
-              <img className='round-img my-1' src={profile.user.avatar} alt='' />
-              <h1 className='large'>{profile.user.name}</h1>
-              <p className='lead'>
-                {profile.status} {profile.company && <span> at {profile.company}</span>}
-              </p>
-              <p>{location}</p>
-              <div className='icons my-1'>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fas fa-globe fa-2x'></i>
-                </a>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-twitter fa-2x'></i>
-                </a>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-facebook fa-2x'></i>
-                </a>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-linkedin fa-2x'></i>
-                </a>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-youtube fa-2x'></i>
-                </a>
-                <a href='#' target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-instagram fa-2x'></i>
-                </a>
-              </div>
-            </div> */}
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </>
       )}
